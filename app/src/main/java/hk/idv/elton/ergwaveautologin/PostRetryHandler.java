f92508ff751fcf85fa49d9efb8d91717ba9fb7e4
Elton Chung <elton328@gmail.com>
2016-03-31 11:37:45 +0800

    Add back PostRetryHandler.java

---
+++ app/src/main/java/hk/idv/elton/ergwaveautologin/PostRetryHandler.java
@@ -0,0 +1,23 @@
+package hk.idv.elton.ergwaveautologin;
+
+import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+
+// Retry handler allowing the retry of POST requests
+public class PostRetryHandler implements HttpRequestRetryHandler {
+
+    static final int RETRY_COUNT = 2;
+
+    @Override
+    public boolean retryRequest(IOException exception, int executionCount,
+            HttpContext context) {
+        if (executionCount >= RETRY_COUNT) {
+            // Do not retry if over max retry count
+            return false;
+        }
+        return true;
+    }
+
+}
